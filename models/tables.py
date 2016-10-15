01ab5541dc295a1e8d33255ab45a779004a28662
Sean Dougher <hi.imsean@yahoo.com>
2016-10-15 14:45:14 -0700

    Add files via upload
    added tables.py

---
+++ models/tables.py
@@ -0,0 +1,25 @@
+import datetime
+
+#Class Table
+db.define_table('class',
+                Field('class_name')
+                )
+#Project Table
+db.define_table('project',
+                Field('project_name'),
+                Field('current_members'),
+                Field('project_info'),
+                Field('needed_skills'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
+                )
+#User Table
+db.define_table('user',
+                Field('user_email'),
+                Field('addtl_info'),
+                Field('skills'),
+                Field('classes'),
+                )
+
+#When submitting a project, the field should not be empty
+db.project.project_name.requires = IS_NOT_EMPTY()
+db.project.project_info.requires = IS_NOT_EMPTY()
