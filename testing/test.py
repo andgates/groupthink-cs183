6148645b91785e74c6f3797238757b32ee153eb5
Sean Dougher <hi.imsean@yahoo.com>
2016-11-22 18:36:46 -0800

    Testing michales databases

--- testing/test.py
+++ testing/test.py
@@ -4,3 +4,9 @@ from gluon.globals import Request
 
 execfile("applications/groupthink/controllers/default.py", globals())
 
+class TestFunctions(unittest.TestCase):
+        def setUp(self):
+            request = Request()
+
+        def testUserfromEmail(self):
+            self.assertEquals("Richard Julig", get_user_name_from_email("rjulig@ucsc.edu"))
