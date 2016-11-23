5788f8eb148dec3a923a49d5db6d8778330277fc
Michael Gates <mjgates@ucsc.edu>
2016-11-22 22:36:53 -0800

    Working test.py

--- testing/test.py
+++ testing/test.py
@@ -6,7 +6,12 @@ execfile("applications/groupthink/controllers/default.py", globals())
 
 class TestFunctions(unittest.TestCase):
         def setUp(self):
-            request = Request()
+            return
 
         def testUserfromEmail(self):
             self.assertEquals("Richard Julig", get_user_name_from_email("rjulig@ucsc.edu"))
+
+
+suite = unittest.TestSuite()
+suite.addTest(unittest.makeSuite(TestFunctions))
+unittest.TextTestRunner(verbosity=2).run(suite)
