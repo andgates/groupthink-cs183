cb20c2bba4ca56bcff4b400357d11f544679dbe4
Sean Dougher <hi.imsean@yahoo.com>
2016-11-23 11:23:36 -0800

    Updated test.py to test all functionality of get_username_from_email

--- testing/test.py
+++ testing/test.py
@@ -7,9 +7,14 @@ execfile("applications/groupthink/controllers/default.py", globals())
 class TestFunctions(unittest.TestCase):
         def setUp(self):
             return
-
+        #tests userfromemail to see if it works
         def testUserfromEmail(self):
+            #Should pass
             self.assertEquals("Richard Julig", get_user_name_from_email("rjulig@ucsc.edu"))
+            #Should fail
+            self.assertEquals("Richy lig", get_user_name_from_email("rjulig@ucsc.edu"))
+            #Should fail
+            self.assertEquals("Richard Julig", get_user_name_from_email("theprofessor@ucsc..edu"))
 
 
 suite = unittest.TestSuite()
